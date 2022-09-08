21dc6a3dceb3c11983dd52c9b2108b97064a837c
BernabasGirma <bernabasgirma00@gmail.com>
2022-09-08 20:42:47 +0300

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
+*/
+
 int main(void)
 {
-    fwrite(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
-    return (0);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
