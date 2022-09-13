a7be7256f1510e85fe2029a6d4876d7d721e18e0
BernabasGirma <bernabasgirma00@gmail.com>
2022-09-13 16:42:58 +0300

    updated

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table
+ * 
+ * Return (0)
+ */
+
+void times_table(void);
+{
+    int n;
+    for (n = 0, n <= 9; n++)
+    {
+        _putchar ('n*9')
+    }
+    return (0);
+}
